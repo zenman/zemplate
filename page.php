b3439e212251e411097b238fc78ca32426c6fb1d
Thad <thad@zenman.com>
2015-07-23 07:53:16 -0600

    Add Password Protect functionality

--- page.php
+++ page.php
@@ -14,17 +14,23 @@
 
 get_header(); ?>
 
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+<?php if ( !post_password_required() ) : ?> 
 
-    <section class="main-torso page-torso">
-        <div class="page-torso__inner">
-            <article class="page-torso__content">
-                <h1><?php the_title(); ?></h1>
-                <?php the_content(); ?>
-            </article><!-- //page-torso__content -->
-        </div><!-- //page-torso__inner -->
-    </section><!-- //page-torso -->
+	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-<?php endwhile; ?>
+	    <section class="main-torso page-torso">
+	        <div class="page-torso__inner">
+	            <article class="page-torso__content">
+	                <h1><?php the_title(); ?></h1>
+	                <?php the_content(); ?>
+	            </article><!-- //page-torso__content -->
+	        </div><!-- //page-torso__inner -->
+	    </section><!-- //page-torso -->
+
+	<?php endwhile; ?>
+
+<?php else : ?>
+    <?php echo my_password_form(); ?>
+<?php endif; ?>
 
 <?php get_footer(); ?>
