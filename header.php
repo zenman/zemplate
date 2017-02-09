9a1fe5d26fae1efe4a2c11d4520daf2367da619b
Nick Evans <nick@zenman.com>
2017-02-09 16:26:54 -0700

    Add IE compatibility fix

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+<meta http-equiv="X-UA-Compatible" content="IE=EDGE" />
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
