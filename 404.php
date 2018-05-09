cf0759d6b4f4078ccd07ae6706916c98fa01384d
ryansalerno <rsalerno@gmail.com>
2018-05-09 13:18:18 -0600

    I really thought everything was fair game in class names now, but .404 wasn't working, so I guess we play it safer

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 
 get_header(); ?>
 
-<main class="site-main 404">
+<main class="site-main page-404 text-center">
 	<h1>These aren't the pages you're looking for</h1>
 	<p>Use the navigation to steer you back in the direction of a real page!</p>
 </main>
