46550be4622f6c6cc9b147390416be6c62681aeb
ryansalerno <rsalerno@gmail.com>
2018-05-09 12:34:19 -0600

    Sticky header JS and Sticky footer CSS

---
+++ src/js/scripts/sticky-header.js
@@ -0,0 +1,21 @@
+// are you going to have a fixed position header?
+
+// * no: delete this file...
+
+// * yes: match its ID to this first selector and we'll offset the element following it (probably MAIN) by the actual height
+//        so you don't have to guess in CSS or resort to giving it a fixed height (yuck)
+
+var header = document.getElementById('sticky-header');
+
+if (header){
+	header_offset();
+	window.addEventListener('resize', recalc_sticky_header, false);
+}
+
+function header_offset() {
+	header.nextElementSibling.style.marginTop = header.offsetHeight + 'px';
+}
+
+var recalc_sticky_header = debounce(function(evt){
+	header_offset();
+}, 100);
