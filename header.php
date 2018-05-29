114a3b985da6b93f4ef5e37ffefcc1d456c5a73f
ryansalerno <rsalerno@gmail.com>
2018-05-29 08:51:25 -0600

    Huh. That meta header was deprecated/obsoleted longer ago than I thought.

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta http-equiv="x-ua-Compatible" content="ie=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 
 	<?php // use this wonderful thing: https://realfavicongenerator.net/ ?>
