65b423d29bb0c152cc7076af9f9671a6bd22dccd
ryansalerno <rsalerno@gmail.com>
2018-08-13 16:23:25 -0600

    Conditional enqueue in templates instead of depending on ACF

--- src/functions/enqueue.php
+++ src/functions/enqueue.php
@@ -27,24 +27,8 @@ function zen_enqueues(){
 	//======================================================================
 	// Conditional Includes
 	//======================================================================
-	// We use ACF a lot, but you can also do some checks for is_front_page() or template names or whatever you like...
-	// Here are some examples that won't do anything but can serve as a template if you'd like
-	if ( function_exists('have_rows') && have_rows( 'modules' ) ){
-		while(have_rows('modules')){
-			the_row();
-
-			switch (get_row_layout()) {
-				case 'caret_slider':
-				case 'some_other_slider':
-					wp_enqueue_style( 'flickity-css', get_template_directory_uri() . '/lib/js/flickity/flickity.min.css', array(), '2.0.10');
-					wp_enqueue_script( 'flickity-js', get_template_directory_uri() . '/lib/js/flickity/flickity.pkgd.min.js', array(), '2.0.10', true );
-					wp_enqueue_script( 'zemplate-sliders', get_template_directory_uri() . '/lib/js/sliders.min.js', array('flickity-js'), $ver, true);
-					break;
-				case 'animated_stats':
-					wp_enqueue_script( 'zemplate-inview', get_template_directory_uri() . '/lib/js/inview.min.js', array('zemplate'), $ver, true);
-					break;
-			}
-		}
-	}
+	// put this in template files as necessary (WP takes care of not adding it to the page more than once):
+	//     wp_enqueue_script( 'whatever' );
+	wp_register_script( 'sliders', get_template_directory_uri() . '/lib/js/sliders.min.js', array('zemplate'), $ver, true);
 }
 add_action('wp_enqueue_scripts', 'zen_enqueues');
