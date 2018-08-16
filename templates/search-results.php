ab529182dccd0017b1ccabad36ae3bf466d04a4d
ryansalerno <rsalerno@gmail.com>
2018-08-16 15:32:00 -0600

    Post type prefixes in search (should have been in the search commit 👇)

--- templates/search-results.php
+++ templates/search-results.php
@@ -1,5 +1,5 @@
 <article class="blog-excerpt less-wide">
-	<h3><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+	<h3><?php echo zen_friendlier_post_type(get_post_type_object(get_post_type())->labels->singular_name); ?> &mdash; <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
 	<section class="post-content">
 		<?php the_excerpt(); ?>
 	</section>
