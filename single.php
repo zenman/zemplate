93782d7842d3a7c280393266ad4700cab39d486e
ryansalerno <rsalerno@gmail.com>
2018-10-18 15:53:20 -0600

    Tags and Categories on single blog pages

--- single.php
+++ single.php
@@ -13,8 +13,25 @@ get_header();
 			<section class="post-meta">
 				<p><?php echo get_the_date();?></p>
 				<?php
+					if (has_category()){
+						$categories = get_the_category();
+						$cat_links = array();
+						foreach ($categories as $category){
+							$cat_links[] = '<a href="'.get_category_link($category).'" title="'.__('View all posts in this category', 'zemplate').'">'.trim(esc_html($category->name)).'</a>';
+						}
+						if ($cat_links){
+							echo '<p class="blog-cats">'.implode(', ', $cat_links).'</p>';
+						}
+					}
 					if (has_tag()){
-						the_tags();
+						$tags = get_the_tags();
+						$tag_links = array();
+						foreach ($tags as $tag){
+							$tag_links[] = '<a href="'.get_tag_link($tag).'" title="'.__('View all posts with this tag', 'zemplate').'">'.trim(esc_html($tag->name)).'</a>';
+						}
+						if ($tag_links){
+							echo '<p class="blog-tags">'.implode(', ', $tag_links).'</p>';
+						}
 					}
 				?>
 			</section>
